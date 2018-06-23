29c480fbc77d8b8f92d9c86e3fb76ffaab2463bd
Simon Tal <simon_tal@hotmail.com>
2018-06-23 18:13:15 +0800

    Add dep:Vuex

--- src/main.js
+++ src/main.js
@@ -3,11 +3,16 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import Vuex from 'vuex'
 import Element from 'element-ui'
+import storeBase from './store'
 
 import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.use(Element)
+Vue.use(Vuex)
+
+const store = new Vuex.Store(storeBase)
 
 Vue.config.productionTip = false
 
@@ -15,6 +20,7 @@ Vue.config.productionTip = false
 new Vue({
   el: '#app',
   router,
+  store,
   components: { App },
   template: '<App/>'
 })
