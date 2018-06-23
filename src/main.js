cc1f3803c3cebab9477b886be63fda2d402b54a6
Simon Tal <simon_tal@hotmail.com>
2018-06-23 17:40:48 +0800

    Add dep:elementui

--- src/main.js
+++ src/main.js
@@ -3,6 +3,11 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import Element from 'element-ui'
+
+import 'element-ui/lib/theme-chalk/index.css'
+
+Vue.use(Element)
 
 Vue.config.productionTip = false
 
